7932eab83915f46284125e66ac2b9782ef89ba80
Julien Maze <Julien.Maze@bbc.co.uk>
2015-04-28 17:00:59 +0100

    WIP

--- app/scripts/controllers/main.controller.js
+++ app/scripts/controllers/main.controller.js
@@ -1,30 +1,12 @@
 'use strict';
 
-angular.module('studylater').controller('MainController', function ($scope) {
-    //$scope.resources = [];
-    $scope.resources = [{
-        title: 'States of Matter - Revision 1',
-        url: 'http://www.bbc.co.uk/education/guides/zccmn39/revision/'
-    }, {
-        title: 'Iron and aluminium - Revision 1',
-        url: 'http://www.bbc.co.uk/education/guides/zfsk7ty/revision'
-    }, {
-        title: 'States of Matter - Revision 1',
-        url: 'http://www.bbc.co.uk/education/guides/zccmn39/revision/'
-    }, {
-        title: 'Iron and aluminium - Revision 1',
-        url: 'http://www.bbc.co.uk/education/guides/zfsk7ty/revision'
-    }, {
-        title: 'States of Matter - Revision 1',
-        url: 'http://www.bbc.co.uk/education/guides/zccmn39/revision/'
-    }, {
-        title: 'Iron and aluminium - Revision 1',
-        url: 'http://www.bbc.co.uk/education/guides/zfsk7ty/revision'
-    }, {
-        title: 'States of Matter - Revision 1',
-        url: 'http://www.bbc.co.uk/education/guides/zccmn39/revision/'
-    }, {
-        title: 'Iron and aluminium - Revision 1',
-        url: 'http://www.bbc.co.uk/education/guides/zfsk7ty/revision'
-    }];
+angular.module('studylater').controller('MainController', function ($scope, StorageService) {
+    $scope.resources = StorageService.get();
+    $scope.removeResource = function (url) {
+        StorageService.remove(url);
+        $scope.resources = StorageService.get();
+        if (!$scope.$$phase) {
+            $scope.$apply();
+        }
+    };
 });
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,3 +1,13 @@
 'use strict';
 
 angular.module('studylater', []);
+
+document
+    .querySelector('.learning-resources__add-button')
+    .addEventListener('click', function () {
+        chrome.tabs.getCurrent(function (tab) {
+            var title = tab.title;
+            var url = tab.url;
+            console.log(title + ': ' + url);
+        });
+    }, false);
